84d8905aac1a7895d3ba5cdc16cd83ef5b6dd640
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-06-12 15:26:34 +0300

    Initial Commit

---
+++ Northstar Demo/Northstar_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Northstar_DemoApp.swift
+//  Northstar Demo
+//
+//  Created by Andreas Näsman on 12.6.2025.
+//
+
+import SwiftUI
+
+@main
+struct Northstar_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Northstar DemoTests/Northstar_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Northstar_DemoTests.swift
+//  Northstar DemoTests
+//
+//  Created by Andreas Näsman on 12.6.2025.
+//
+
+import Testing
+@testable import Northstar_Demo
+
+struct Northstar_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
