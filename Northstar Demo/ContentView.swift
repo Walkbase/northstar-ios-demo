71a0487371452cab1053141bff9179391de49070
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-06-19 13:39:51 +0300

    Demo SDK

--- Northstar Demo/ContentView.swift
+++ Northstar Demo/ContentView.swift
@@ -1,12 +1,17 @@
 import SwiftUI
+import Northstar
 
 struct ContentView: View {
+    private let demo = Demo()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Demo") {
+                self.demo.greet()
+            }
         }
         .padding()
     }
