4bfa91489e7051477e036f2a916e873db31aeee5
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-06-24 14:50:46 +0300

    Revert "Add a first build of the Northstar SDK"

--- Northstar Demo/ContentView.swift
+++ Northstar Demo/ContentView.swift
@@ -1,21 +1,12 @@
-import Northstar
 import SwiftUI
 
 struct ContentView: View {
-    private let positioning = WalkbasePositioning()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Button("SDK test") {
-                positioning.test()
-            }
-            .padding()
-            .background(.blue)
-            .foregroundStyle(.white)
-            .clipShape(.capsule)
         }
         .padding()
     }
