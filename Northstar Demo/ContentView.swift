723678b1bd34a77bc7ba6068eb14692460d91691
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-09-22 08:53:39 +0300

    Rename button

--- Northstar Demo/ContentView.swift
+++ Northstar Demo/ContentView.swift
@@ -7,7 +7,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Group {
-                Button("Set Up") {
+                Button("Show Login") {
                     showLoginView = true
                 }
                 Button("Show Map") {
