d621595868209a25550faf6248e39a556d94b90a
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2026-05-25 13:48:37 +0300

    Add wrapping VStack

--- Northstar Demo/BasicUsageView.swift
+++ Northstar Demo/BasicUsageView.swift
@@ -14,112 +14,115 @@ struct BasicUsageView: View {
     @State private var showAlert = false
 
     var body: some View {
-        Grid(alignment: .leading) {
-            GridRow {
-                var isDisabled: Bool { positioning != nil }
+        VStack(alignment: .leading) {
+            Grid(alignment: .leading) {
+                GridRow {
+                    var isDisabled: Bool { positioning != nil }
 
-                Text("Region:")
-                Picker("Region", selection: $selectedRegion) {
-                    ForEach(Northstar.Region.allCases, id: \.self) { region in
-                        Text(region.rawValue.uppercased()).tag(region)
-                    }
-                }
-                .border(.blue)
-                .disabled(isDisabled)
-                .opacity(isDisabled ? 0.5 : 1)
-            }
-
-            GridRow {
-                var isDisabled: Bool { positioning != nil }
-
-                Text("API Key:")
-                TextField("", text: $apiKey)
-                    .border(.blue)
-                    .submitLabel(.go)
-                    .disabled(isDisabled)
-                    .opacity(isDisabled ? 0.5 : 1)
-            }
-            .onSubmit { Task { await start() } }
-
-            GridRow {
-                var canStart: Bool {
-                    positioning == nil || positioning?.status == .stopped
-                }
-
-                Button {
-                    Task { canStart ? await start() : stop() }
-                } label: {
-                    Group {
-                        if isLoading {
-                            ProgressView().tint(.white)
-                        } else {
-                            Text(canStart ? "Start" : "Stop")
+                    Text("Region:")
+                    Picker("Region", selection: $selectedRegion) {
+                        ForEach(Northstar.Region.allCases, id: \.self) {
+                            region in
+                            Text(region.rawValue.uppercased()).tag(region)
                         }
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.blue)
-                    .textCase(.uppercase)
-                    .fontWeight(.bold)
-                }
-                .gridCellColumns(2)
-            }
-
-            GridRow {
-                var isDisabled: Bool {
-                    positioning == nil || positioning?.status != .stopped
+                    .border(.blue)
+                    .disabled(isDisabled)
+                    .opacity(isDisabled ? 0.5 : 1)
                 }
 
-                Button {
-                    positioning = nil
-                } label: {
-                    Text("Reset")
+                GridRow {
+                    var isDisabled: Bool { positioning != nil }
+
+                    Text("API Key:")
+                    TextField("", text: $apiKey)
+                        .border(.blue)
+                        .submitLabel(.go)
+                        .disabled(isDisabled)
+                        .opacity(isDisabled ? 0.5 : 1)
+                }
+                .onSubmit { Task { await start() } }
+
+                GridRow {
+                    var canStart: Bool {
+                        positioning == nil || positioning?.status == .stopped
+                    }
+
+                    Button {
+                        Task { canStart ? await start() : stop() }
+                    } label: {
+                        Group {
+                            if isLoading {
+                                ProgressView().tint(.white)
+                            } else {
+                                Text(canStart ? "Start" : "Stop")
+                            }
+                        }
                         .frame(maxWidth: .infinity)
                         .padding()
                         .foregroundStyle(.white)
                         .background(.blue)
                         .textCase(.uppercase)
                         .fontWeight(.bold)
+                    }
+                    .gridCellColumns(2)
                 }
-                .gridCellColumns(2)
-                .disabled(isDisabled)
-                .opacity(isDisabled ? 0.5 : 1)
-            }
 
-            Divider().hidden()
+                GridRow {
+                    var isDisabled: Bool {
+                        positioning == nil || positioning?.status != .stopped
+                    }
 
-            GridRow {
-                Text("Status:")
-                Text(positioningStatus)
-            }
-            .onChange(of: positioning?.status) { _, status in
-                switch status {
-                case .none:
-                    positioningStatus = "Positioning not started"
-                case .stopped:
-                    positioningStatus = "⏹️ Positioning stopped"
-                case .connectingToStream, .reconnecting, .starting,
-                    .waitingForNetwork:
-                    positioningStatus = "⏳ Positioning starting"
-                case .waitingForUpdates:
-                    positioningStatus = "⏳ Finding your position"
-                case .receivingUpdates:
-                    positioningStatus = "✅ Tracking your position"
-                @unknown default:
-                    positioningStatus = "❌ Something went wrong"
+                    Button {
+                        positioning = nil
+                    } label: {
+                        Text("Reset")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .foregroundStyle(.white)
+                            .background(.blue)
+                            .textCase(.uppercase)
+                            .fontWeight(.bold)
+                    }
+                    .gridCellColumns(2)
+                    .disabled(isDisabled)
+                    .opacity(isDisabled ? 0.5 : 1)
                 }
-            }
 
-            GridRow(alignment: .top) {
-                Text("Position:")
-                Group {
-                    if let position = positioning?.position {
-                        Text(
-                            "Timestamp: \(position.timestamp)\nLatitude: \(position.latitude)\nLongitude: \(position.longitude)"
-                        )
-                    } else {
-                        Text("None received yet")
+                Divider().hidden()
+
+                GridRow {
+                    Text("Status:")
+                    Text(positioningStatus)
+                }
+                .onChange(of: positioning?.status) { _, status in
+                    switch status {
+                    case .none:
+                        positioningStatus = "Positioning not started"
+                    case .stopped:
+                        positioningStatus = "⏹️ Positioning stopped"
+                    case .connectingToStream, .reconnecting, .starting,
+                        .waitingForNetwork:
+                        positioningStatus = "⏳ Positioning starting"
+                    case .waitingForUpdates:
+                        positioningStatus = "⏳ Finding your position"
+                    case .receivingUpdates:
+                        positioningStatus = "✅ Tracking your position"
+                    @unknown default:
+                        positioningStatus = "❌ Something went wrong"
+                    }
+                }
+
+                GridRow(alignment: .top) {
+                    Text("Position:")
+                    Group {
+                        if let position = positioning?.position {
+                            Text(
+                                "Timestamp: \(position.timestamp)\nLatitude: \(position.latitude)\nLongitude: \(position.longitude)"
+                            )
+                        } else {
+                            Text("None received yet")
+                        }
                     }
                 }
             }
